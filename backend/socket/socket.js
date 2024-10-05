4cb7fd2b7a7f9100ccad02bedb2e60bf37d16028
kumarinisha2715@gmail.com <kumarinisha2715@gmail.com>
2024-10-05 18:03:17 +0530

    4th part complete

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -36,3 +36,58 @@ io.on("connection", (socket) => {
 });
 
 export { app, io, server };
+
+
+// import { Server } from "socket.io";
+// import http from "http";
+// import express from "express";
+
+// const app = express();
+
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:3000"],
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// const userSocketMap = {}; // {userId: socketId}
+
+// export const getReceiverSocketId = (receiverId) => {
+//   return userSocketMap[receiverId];
+// };
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected", socket.id);
+
+//   const userId = socket.handshake.query.userId;
+//   if (userId !== undefined && userId) {
+//     userSocketMap[userId] = socket.id;
+//   }
+
+//   // Emit online users to all connected clients
+//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//   // Handle sending messages
+//   socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+//     const receiverSocketId = getReceiverSocketId(receiverId);
+//     if (receiverSocketId) {
+//       io.to(receiverSocketId).emit("receiveMessage", {
+//         senderId,
+//         message,
+//       });
+//     } else {
+//       console.log("Receiver is not online");
+//     }
+//   });
+
+//   // Handle user disconnection
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected", socket.id);
+//     delete userSocketMap[userId];
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   });
+// });
+
+// export { app, io, server };
